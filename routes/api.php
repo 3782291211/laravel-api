e7afb527d07586dd09eacf6a99014ba47c24ada1
3782291211 <Laklouk98@hotmail.com>
2023-04-22 21:15:58 +0100

    Rearranging routes

--- routes/api.php
+++ routes/api.php
@@ -15,27 +15,21 @@ use App\Http\Controllers\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 // Public routes
-Route::get('/exams/{id}', [ExamController::class, 'show']);
-Route::get('/exams/search/{name}', [ExamController::class, 'search']);
-
-// calls the signup method on the auth controller
-//Route::post('/login', fn ($id) => User::where());
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/signup', [AuthController::class, 'signup']);
 
 
 // Protected routes - require tokens
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/exams/{id}', [ExamController::class, 'show']);
+    Route::get('/exams/search/{name}', [ExamController::class, 'search']);
+    Route::get('/exams', [ExamController::class, 'examsIndex']);
     Route::get('/users', [UserController::class, 'index']);
-    Route::get('/exams', [ExamController::class, 'index']);
+    Route::get('/users/{id}/exams', [UserController::class, 'userExams']);
     Route::post('/exams', [ExamController::class, 'store']);
     Route::put('/exams/{id}', [ExamController::class, 'update']);
     Route::delete('/exams/{id}', [ExamController::class, 'destroy']);
-    // logout
     Route::get('/logout/{id}', [AuthController::class, 'logout']);
 });
-
-//Route::middleware('auth:sanctum')
-   // ->get('/user', fn (Request $request) => $request->user());
