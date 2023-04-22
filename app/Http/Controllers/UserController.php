8207513909d415c6633608bca53a737e388705a8
3782291211 <Laklouk98@hotmail.com>
2023-04-22 21:13:03 +0100

    Adding method for individual user's exams

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Exam;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return User::all();
+        if (!Gate::allows('view-users')) {
+            return response(['msg' => 'Administrator access is required to perform this action.'], 403);
+        }
+
+        return response(['users' => User::all()]);
+    }
+
+    public function userExams(int $id)
+    {
+        return response([
+            'exams' => Exam::where('candidate_id' , '=', $id)
+                            ->orderBy('date', 'asc')
+                            ->get()
+        ]);
     }
 }
