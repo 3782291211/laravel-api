3db4c6f4da91efa1d0fb74e95e4f682888aeadba
3782291211 <Laklouk98@hotmail.com>
2023-04-22 21:14:20 +0100

    Adding in one-to-many relationship on Exam model

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Exam extends Model
 {
     use HasFactory;
-    # we can specify a name for our table:
-    # protected $table = 'my_products';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public $timestamps = false;
 
