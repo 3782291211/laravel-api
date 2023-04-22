1e21e7fbbd8cf133446bc81e39603849fc6e7a69
3782291211 <Laklouk98@hotmail.com>
2023-04-22 21:15:49 +0100

    Adding exams to factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\Exam;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()
+            ->has(Exam::factory()->count(4))
             ->count(10)
             ->create();
     }
