d5efe7083a4d70e0616e8414d9dbbc2382d46171
3782291211 <Laklouk98@hotmail.com>
2023-04-24 14:28:15 +0100

    Moving class to new file

--- database/seeders/ExamSeeder.php
+++ database/seeders/ExamSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Exam;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class ExamSeeder extends Seeder
 {
@@ -17,31 +15,5 @@ class ExamSeeder extends Seeder
         Exam::factory()
             ->count(50)
             ->create();
-    }
-}
-
-class JsonSeeder extends Seeder
-{
-    public function run(): void
-    {
-        $json = Storage::disk('local')->get('/json/candidate_data.json');
-        $candidates = json_decode($json, true);
-
-        if (is_array($candidates) || is_object($candidates))
-        {
-            foreach ($candidates as $candidate)
-            {
-                DB::table('exams')->insert([
-                    "title" => $candidate['title'],
-                    "description" => $candidate['description'],
-                    "candidate_id" => $candidate['candidate_id'],
-                    "candidate_name" => $candidate['candidate_name'],
-                    "date" => $candidate['date'],
-                    "location_name" => $candidate['location_name'],
-                    "latitude" => $candidate['latitude'],
-                    "longitude" => $candidate['longitude']
-                ]);
-            }
-        }
     }
 }
