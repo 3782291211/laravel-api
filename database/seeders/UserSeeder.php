33c4fe5c1d0e996723b9ccfe0cbc2d8da29adc79
3782291211 <Laklouk98@hotmail.com>
2023-04-24 14:28:58 +0100

    Adding closure-based state transformation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,11 +11,21 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
         User::factory()
-            ->has(Exam::factory()->count(4))
-            ->count(10)
+            ->count(21)
+            ->has(
+                Exam::factory()
+                        ->count(fake()->numberBetween(2, 22))
+                        ->state(fn (array $attributes, User $user) => 
+                            [
+                                'candidate_id' => $user->id,
+                                'candidate_name' => $user->name
+                            ]
+                        )
+            )
             ->create();
     }
 }
